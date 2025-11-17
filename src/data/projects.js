395040a799d0a2edf97520990363f29051eab9ab
James Hughes <jhughes2712@gmail.com>
2025-11-17 00:05:09 +0000

    Chore: Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,67 @@
 const projects = [
   {
     id: "project-1",
+    title: "Rate My Bookshelf",
+    description: "Give users information about a bookshelf (Docker, Image Segmentation/OCR, Google Cloud)",
+    imageUrl: "/book-hero.png",
+    details: "Description incoming, for now visit the GitHub or the live site.",
+    github: "https://github.com/james-hughes1/rate-my-bookshelf-backend",
+    websitelink: "https://rate-my-bookshelf.vercel.app/"
+  },
+  {
+    id: "project-2",
+    title: "Robo Dunk",
+    description: "Play poker with AI opponents. (AWS & Monitoring, Reinforcement Learning, CI/CD)",
+    imageUrl: "/robodunk-small.png",
+    details: "Description incoming, for now visit the GitHub or the live site.",
+    github: "https://github.com/james-hughes1/robo-dunk",
+    websitelink: "https://robot-sandbox.xyz/"
+  },
+  {
+    id: "project-3",
+    title: "Microscopy with AI",
+    description: "Research with Cambridge Biotech.",
+    imageUrl: "/sim-small.png",
+    details: "Description incoming, for now visit the GitHub or read the exec summary.",  
+    github: "https://github.com/james-hughes1/sim_denoising",
+    websitelink: "https://https://github.com/james-hughes1/sim_denoising/blob/main/report/executive_summary_jh2284.pdf"
+  },
+  {
+    id: "project-4",
+    title: "Game of Life",
+    description: "Supercomputer simulations. (C++, Parallelism/HPC/SLURM)",
+    imageUrl: "/gol-small.png",
+    details: "Description incoming, for now visit the GitHub or read the report.",
+    github: "https://github.com/james-hughes1/game-of-life-hpc",
+    websitelink: "https://github.com/james-hughes1/game-of-life-hpc/blob/main/report/c2_assessment_jh2284.pdf"
+  },
+  {
+    id: "project-5",
+    title: "Diffusion MNIST",
+    description: "Image generation. (Doxygen, PyTorch, Generative AI)",
+    imageUrl: "/diffusion.png",
+    details: "Description incoming, for now visit the GitHub or read the report.",  
+    github: "https://github.com/james-hughes1/diffusion-mnist/",
+    websitelink: "https://github.com/james-hughes1/diffusion-mnist/blob/main/report/m2_assessment_jh2284.pdf"
+  },
+  {
+    id: "project-6",
     title: "Flag Puzzle",
-    description: "A fast-paced flag guessing game (Javascript, MongoDB, Frontend)",
+    description: "A fast-paced flag guessing game. (Javascript, MongoDB, Frontend)",
     imageUrl: "/flagpuzzleimg.png",
-    details: "A flag puzzle game.",  
+    details: "Description incoming, for now visit the GitHub or visit the site.",  
     github: "https://github.com/james-hughes1/flagpuzzle",
     websitelink: "https://flagpuzzle.onrender.com"
   },
   {
-    id: "project-2",
+    id: "project-7",
     title: "Poker Simulator",
     description: "Play poker with AI opponents. (LLM APIs, React)",
     imageUrl: "/pokersimimg.png",
-    details: "Here's how I did it.",
+    details: "Description incoming, for now visit the GitHub or visit the site.",
     github: "https://github.com/james-hughes1/pokersim",
     websitelink: "https://pokersim.vercel.app/"
   },
-  {
-    id: "project-3",
-    title: "Placeholder",
-    description: "A clean and simple personal blogging platform.",
-    imageUrl: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=800&q=80",
-    details: "Here's how I did it."
-  },
-  {
-    id: "project-4",
-    title: "Placeholder",
-    description: "Search and save your favorite recipes.",
-    imageUrl: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=800&q=80",
-    details: "Here's how I did it."
-  }
 ];
 
 export default projects;
